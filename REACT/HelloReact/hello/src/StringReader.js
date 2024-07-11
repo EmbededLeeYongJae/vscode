52bd379a63470e4341d6fc7f0b12742662f6f5df
EmbededLeeYongJae <dydwo6018@gmail.com>
2024-07-11 17:47:58 +0900

    87654654567

---
+++ REACT/HelloReact/hello/src/StringReader.js
@@ -0,0 +1,18 @@
+import StringOperator from "./StringOperator";
+
+export default function StringReader() {
+
+  return (
+
+        <>
+            <StringOperator onChangeMode={
+                function(e){
+                    console.log('문자열 길이 : ' + e.target.value.length);
+                }           
+             } />
+
+        </>
+    
+  );
+}
+
